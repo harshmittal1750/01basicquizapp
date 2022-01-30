0dda3cd0cb39243a0c06f7ae2a5fa274c4114289
Harsh Mittal <harshmittal1750@gmail.com>
2022-01-31 02:56:04 +0530

    basic idea completion with setscore,showscore,eventhandler and ui fixed

--- src/Quiz.js
+++ src/Quiz.js
@@ -3,24 +3,49 @@ import "./Quiz.css";
 import { questions } from "./questions";
 const Quiz = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+  const handleAnswerButtonClick = (isCorrect) => {
+    if (isCorrect === true) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
   return (
-    <div>
-      <div className="container d-flex quiz-box">
-        <div className="lg-6 quiz-box-1">
-          <span style={{ fontSize: "2.5vw" }}>Question 1/</span>
-          {questions.length}
-          <div className="ques-content">
-            {questions[currentQuestion].questionText}
-          </div>
+    <div className="container d-flex justify-content-center quiz-box">
+      {showScore ? (
+        <div
+          style={{ fontSize: "50px", marginTop: "auto", marginBottom: "auto" }}
+        >
+          You scored out of {score} out of {questions.length}
         </div>
-        <div className="lg-5 quiz-box-2  ">
-          <div className="questions">
+      ) : (
+        <div className="row">
+          <div className="col-sm-6 quesField">
+            <span style={{ fontSize: "2.5vw" }}>Question 1/</span>
+            {questions.length}
+            <div className="ques-content">
+              {questions[currentQuestion].questionText}
+            </div>
+          </div>
+          <div className="col-sm-6 ansField  ">
             {questions[currentQuestion].answerOption.map((answerOption) => (
-              <button className="ans-btn">{answerOption.answerText}</button>
+              <button
+                onClick={() => handleAnswerButtonClick(answerOption.isCorrect)}
+                className="ans-btn"
+              >
+                {answerOption.answerText}
+              </button>
             ))}
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
